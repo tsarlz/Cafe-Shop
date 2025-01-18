8e4d5b207250f3a6ea58f84157f455db39fe9ddf
charles1stgithub <charleslester.official@gmail.com>
2025-01-18 21:45:13 +0800

    Navbar and Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../components/navbar/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'langgacafe';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home/home.component';
+import { MenuComponent } from '../pages/menu/menu/menu.component';
+import { LocationComponent } from '../pages/location/location/location.component';
+import { FeedbackComponent } from '../pages/feedback/feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent,
+  },
+  {
+    path: 'location',
+    component: LocationComponent,
+  },
+];
